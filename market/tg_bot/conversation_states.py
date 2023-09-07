be063b989b32ad55d84a0accb7fa0428c22a7ab0
@vadim_veshkin <vadim.veshkin@mail.ru>
2023-09-08 00:06:09 +0400

    States enum is created

---
+++ market/tg_bot/conversation_states.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class States(Enum):
+    CHOOSE_ROLE = 1
+    LOGIN = 2
+    PASSWORD = 3
+    SHOP_LIST = 4
+    API_KEY = 5
+    SHOP_MENU = 6
+    SHOP_INFO = 7
+    ACTIVATE = 8
+    PRICE_UPDATING = 9
