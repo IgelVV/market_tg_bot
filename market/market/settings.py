d2037561f62fbc63650c639808ba492d30b3e83a
@vadim_veshkin <vadim.veshkin@mail.ru>
2023-09-07 15:58:04 +0400

    tg_bot app has been created

--- market/market/settings.py
+++ market/market/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'tg_bot.apps.TgBotConfig',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Telegram bot Settings
+
+TG_BOT_TOKEN = ...
