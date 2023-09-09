b2d966792ce6f6b7a8b056bb83db209c508fe051
@vadim_veshkin <vadim.veshkin@mail.ru>
2023-09-09 21:28:40 +0400

    auth services are added

--- market/tg_bot/services/user_services.py
+++ market/tg_bot/services/user_services.py
@@ -1,20 +1,64 @@
+import logging
+
+from asgiref.sync import sync_to_async
+
 from telegram.ext import (
     ContextTypes,
 )
+from django.contrib.auth import get_user_model
 
-# roles
-ADMIN = "admin"
-SELLER = "seller"
+from shop.models import Shop
 
+UserModel = get_user_model()
 
-def is_authenticated(context: ContextTypes.DEFAULT_TYPE) -> bool:
-    return context.user_data.get("authenticated", False)
-
-
-def get_role(context: ContextTypes.DEFAULT_TYPE):
-    # find role in db
-    return context.user_data.get('role')
+logger = logging.getLogger(__name__)
 
 
-def set_role(context: ContextTypes.DEFAULT_TYPE):
-    ...
+class UserService:
+    ADMIN_ROLE = "admin"
+    SELLER_ROLE = "seller"
+
+    AUTH_KEY = "authenticated"
+    ROLE_KEY = "role"
+
+    def __init__(self, context: ContextTypes.DEFAULT_TYPE):
+        self.context = context
+
+    def is_authenticated(self) -> bool:
+        return self.context.user_data.get("authenticated", False)
+
+    def get_role(self):
+        # find role in db
+        return self.context.user_data.get('role')
+
+    def set_role(self):
+        ...
+
+    @sync_to_async
+    def authenticate_admin(
+            self,
+            username: str,
+            password: str,
+            tg_user_id: int,
+    ):
+        # tg_user_id for saving to db
+        try:
+            user = UserModel.objects.get(username=username)
+            if user.check_password(password):
+                self.context.user_data[self.AUTH_KEY] = True
+                self.context.user_data[self.ROLE_KEY] = self.ADMIN_ROLE
+                return True
+
+        except UserModel.DoesNotExist:
+            logger.info(f"User with {username=} DoesNotExist")
+            return False
+
+    @sync_to_async
+    def check_shop_api_key(self, shop_api_key: str, tg_user_id: int):
+        # todo use django service layer to access models
+        shop = Shop.objects.filter(api_key=shop_api_key).first()
+        if shop:
+            return True
+        else:
+            return False
+        # todo confirmation by admin
