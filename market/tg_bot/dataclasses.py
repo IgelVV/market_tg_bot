adb29b0dc64fdde9f6a99c5642c1be8a9e9ae318
@vadim_veshkin <vadim.veshkin@mail.ru>
2023-09-10 21:08:17 +0400

    ShopInfo is extended

--- market/tg_bot/dataclasses.py
+++ market/tg_bot/dataclasses.py
@@ -1,10 +1,17 @@
 from dataclasses import dataclass
+from typing import Optional
 
 
 @dataclass
 class ShopInfo:
     id: int
     name: str
+    slug: Optional[str] = None
+    api_key: Optional[str] = None
+    vendor_name: Optional[str] = None
+    is_active: Optional[bool] = None
+    stop_updated_price: Optional[bool] = None
+    individual_updating_time: Optional[bool] = None
 
 
 @dataclass
