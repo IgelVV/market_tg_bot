faf12ca3dd6ba86ddffa09e4403f1e3f053bff6d
@vadim_veshkin <vadim.veshkin@mail.ru>
2023-09-19 21:53:20 +0400

    ShopInfo representation is changed

--- market/tg_bot/dataclasses.py
+++ market/tg_bot/dataclasses.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Optional
 
 
@@ -7,12 +7,12 @@ class ShopInfo:
     """Represents Shop model for using in bot handlers."""
     id: int
     name: str
-    slug: Optional[str] = None
-    api_key: Optional[str] = None
-    vendor_name: Optional[str] = None
-    is_active: Optional[bool] = None
-    stop_updated_price: Optional[bool] = None
-    individual_updating_time: Optional[bool] = None
+    slug: Optional[str] = field(default=None, repr=False)
+    api_key: Optional[str] = field(default=None, repr=False)
+    vendor_name: Optional[str] = field(default=None, repr=False)
+    is_active: Optional[bool] = field(default=None, repr=False)
+    stop_updated_price: Optional[bool] = field(default=None, repr=False)
+    individual_updating_time: Optional[bool] = field(default=None, repr=False)
 
 
 @dataclass
