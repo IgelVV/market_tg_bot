1d147b3c434f2e9a479c8231c1773904f82faf1b
@vadim_veshkin <vadim.veshkin@mail.ru>
2023-09-23 23:58:12 +0400

    ban/unban signals (is refactored)

--- market/tg_bot/models.py
+++ market/tg_bot/models.py
@@ -32,24 +32,21 @@ def save_user_cart(sender, instance: TelegramUser, **kwargs):
     if instance.pk is None:
         pass
     else:
-        import asyncio
-        from telegram.ext import ApplicationBuilder
-
-        from django.conf import settings
-
-        from tg_bot import texts
-
-        application = ApplicationBuilder() \
-            .concurrent_updates(False) \
-            .token(settings.TG_BOT_TOKEN) \
-            .build()
-
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-
         current = instance
         previous: TelegramUser = TelegramUser.objects.get(pk=instance.pk)
         if previous.is_banned != current.is_banned:
+            import asyncio
+            from telegram.ext import ApplicationBuilder
+            from django.conf import settings
+            from tg_bot import texts
+
+            application = ApplicationBuilder() \
+                .concurrent_updates(False) \
+                .token(settings.TG_BOT_TOKEN) \
+                .build()
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+
             if current.is_banned:
                 coroutine = application.bot.send_message(instance.chat_id,
                                                          texts.DISPLAY_BAN)
