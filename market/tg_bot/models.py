f04c810e91336e145ac6cea5184a7c9f321c78f3
@vadim_veshkin <vadim.veshkin@mail.ru>
2023-09-28 13:15:51 +0400

    signal and task moved to other modules

--- market/tg_bot/models.py
+++ market/tg_bot/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from django.db.models.signals import pre_save
-from django.dispatch import receiver
 
 from shop.models import Shop
 
@@ -25,36 +23,3 @@ class TelegramUser(models.Model):
     is_banned = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)  # for subscription
     is_logged_out = models.BooleanField(default=False)  # for refreshing data
-
-
-@receiver(pre_save, sender=TelegramUser)
-def save_user_cart(sender, instance: TelegramUser, **kwargs):
-    if instance.pk is None:
-        pass
-    else:
-        current = instance
-        previous: TelegramUser = TelegramUser.objects.get(pk=instance.pk)
-        if previous.is_banned != current.is_banned:
-            try:
-                send_ban_unban_notification(current)
-            except Exception as e:
-                print(e.__repr__())
-
-
-def send_ban_unban_notification(tg_user: TelegramUser):
-    import asyncio
-    from telegram.ext import ApplicationBuilder
-    from django.conf import settings
-    from tg_bot import texts
-
-    application = ApplicationBuilder() \
-        .concurrent_updates(False) \
-        .token(settings.TG_BOT_TOKEN) \
-        .build()
-
-    if tg_user.is_banned:
-        text = texts.DISPLAY_BAN
-    else:
-        text = texts.DISPLAY_UNBAN
-    coroutine = application.bot.send_message(tg_user.chat_id, text)
-    asyncio.run(coroutine)
