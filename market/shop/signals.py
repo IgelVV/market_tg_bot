f93f3c2555508e3d1e9fd5006b54f124641b2b0a
@vadim_veshkin <vadim.veshkin@mail.ru>
2023-10-26 14:49:22 +0400

    only_if_enabled decorator for signals is added

--- market/shop/signals.py
+++ market/shop/signals.py
@@ -1,24 +1,59 @@
 import asyncio
 import logging
 
+from aio_pika import DeliveryMode, Message
+from django.core import serializers
+from django.conf import settings
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from .models import Shop
-from django.core import serializers
-
-from aio_pika import DeliveryMode, Message
-
 from rabbit import broker
+
+from .models import Shop
 
 logger = logging.getLogger(__name__)
 
+ENABLE_SIGNALS_TO_SYNCHRONISE_DB = settings.ENABLE_SIGNALS_TO_SYNCHRONISE_DB
 
+
+def only_if_enabled(signal, enabled, **kwargs):
+    """
+    Decorator for signals, to use right before `@receiver`
+    decorator to cancel connection.
+
+    Use to disable some signals during specific working mode.
+    E.g. disable signals depending on message broker while migrations
+    or loading fixtures.
+    ```
+    @only_if_enabled(post_save, ENABLE_SIGNALS_TO_SYNCHRONISE_DB, sender=Shop)
+    @receiver(post_save, sender=Shop)
+    def create_or_update_shop(sender, instance, created, **kwargs):
+        ...
+    ```
+    :param signal:
+    :param enabled:
+    :param kwargs:
+    :return:
+    """
+    def _decorator(func):
+        if enabled:
+            return func
+        else:
+            if isinstance(signal, (list, tuple)):
+                for s in signal:
+                    s.disconnect(func, **kwargs)
+            else:
+                signal.disconnect(func, **kwargs)
+            return func
+
+    return _decorator
+
+
+@only_if_enabled(post_save, ENABLE_SIGNALS_TO_SYNCHRONISE_DB, sender=Shop)
 @receiver(post_save, sender=Shop)
 def create_or_update_shop(sender, instance, created, **kwargs):
     """
     Signal triggered when the Shop object is created or updated
     """
-    # todo add flag to settings `disable_signals` and write decorator.
     logger.info("Shop post save signal")
 
     data = serializers.serialize("json", [instance])
@@ -42,6 +77,7 @@ def create_or_update_shop(sender, instance, created, **kwargs):
     asyncio.run(coro)
 
 
+@only_if_enabled(post_delete, ENABLE_SIGNALS_TO_SYNCHRONISE_DB, sender=Shop)
 @receiver(post_delete, sender=Shop)
 def delete_shop(sender, instance, **kwargs):
     """
